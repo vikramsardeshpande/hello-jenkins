002c4665afc3e69796ff37379db4e8c78cc56e72
cicd <cicd@testvm3.(none)>
2015-06-23 22:03:51 +0530

    modified deploy script

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkis');
+  res.send('hello persistent');
 });
  
 app.listen(process.env.PORT || 5000);
