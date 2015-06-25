c709db7f9a2e3081a5b1f5e62b339cfcfef862e4
cicd-deploy <cicd-deploy@testvm3.(none)>
2015-06-25 20:53:05 +0530

    modified node.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello persistent');
+  res.send('hello Harish .. this is demo');
 });
  
 app.listen(process.env.PORT || 5000);
